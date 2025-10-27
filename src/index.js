1430d0f6c38fcf48d33aba646010ea40882753c5
goldenvertex42 <fabled93@gmail.com>
2025-10-27 13:38:35 -0500

    Entry point where images can be imported and added to the images array and the carousel is initiated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import "./styles.css";
+import { carousel } from "./carousel";
+import image1 from "./assets/image1.jpeg";
+import image2 from "./assets/image2.jpeg";
+import image3 from "./assets/image3.jpeg";
+import image4 from "./assets/image4.jpg";
+import image5 from "./assets/image5.jpeg";
 
-console.log("This is goldenvertex's default template");
+document.addEventListener('DOMContentLoaded', () => {
+  const images = [image1, image2, image3, image4, image5];
+
+  const myCarousel = carousel(
+    '.container',
+    images,
+    '.right-arrow',
+    '.left-arrow'
+  );
+
+  myCarousel.init();
+});
